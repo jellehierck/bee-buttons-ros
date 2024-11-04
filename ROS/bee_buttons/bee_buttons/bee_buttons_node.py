8f83837adc7693dd3310732a5dccfff069c89621
Jelle Hierck <j.j.hierck@student.utwente.nl>
2024-11-04 12:44:56 +0100

    Added serial line reader helper class and extended ros node

--- ROS/bee_buttons/bee_buttons/bee_buttons_node.py
+++ ROS/bee_buttons/bee_buttons/bee_buttons_node.py
@@ -1,44 +1,97 @@
+import serial
+import serial.tools.list_ports
+
 import rclpy
 from rclpy.node import Node
 
-import rclpy.parameter
-from std_msgs.msg import String
 from rcl_interfaces.msg import ParameterDescriptor, FloatingPointRange
+import std_msgs.msg
+
+from .periodic_serial_line_reader import PeriodicSerialLineReader
 
 
 class BeeButtonsNode(Node):
+    DEFAULT_BAUD_RATE = 115200
+    DONGLE_DEVICE_NAME = "USB JTAG/serial debug unit"
+
+    BROADCAST_COMMAND = "Broadcast"
+
+    COMMAND_SEPARATOR = ":"
+    COMMAND_END = "`"
+
     def __init__(self) -> None:
         super().__init__("bee_buttons")
-        self.publisher_ = self.create_publisher(String, "topic", 10)
 
-        self.declare_parameter(
+        # Declare all parameters
+        update_rate_param = self.declare_parameter(
             "update_rate",
             100.0,
             ParameterDescriptor(
-                description="Rate at which the button states are polled",
+                description="Rate (Hz) at which the button states are polled",
                 read_only=True,  # Rate cannot be changed after the node is initialized
                 floating_point_range=[
                     FloatingPointRange(
                         from_value=0.001,  # Allow only values >0
-                        to_value=1000.0,
+                        to_value=1_000_000.0,  # Some large number, required for the FloatingPointRange
                     ),
                 ],
             ),
         )
 
-        update_rate_hz = (
-            self.get_parameter("update_rate").get_parameter_value().double_value
-        )
+        # Serial connection to the dongle
+        try:
+            self.serial_socket = self.open_serial_to_dongle()  # Raises if the connection cannot be opened
+        except serial.SerialException as err:
+            self.get_logger().error("Failed to connect to dongle: %s", err)
+            raise err
+        self.get_logger().info("Connected to dongle on COM port %s", self.serial_socket.port)
+        self.command_broadcast("Battery:Percentage")  # TODO: Why?
+        self.serial_line_reader = PeriodicSerialLineReader(self.serial_socket)
 
+        # Create publishers
+        self.publisher = self.create_publisher(std_msgs.msg.String, "topic", 10)
+
+        # Start timer to poll the serial port
+        update_rate_hz = self.get_parameter(update_rate_param.name).get_parameter_value().double_value
         self.timer = self.create_timer(1 / update_rate_hz, self.timer_callback)
-        self.i = 0
+
+    @classmethod
+    def open_serial_to_dongle(cls, baudrate: int = DEFAULT_BAUD_RATE) -> serial.Serial:
+        """
+        Open a serial socket to the interaction button dongle.
+
+        :return: An open PySerial `Serial` socket which is connected to the interaction button dongle.
+        :raises `serial.SerialException`: If the dongle cannot be found or the serial port cannot be opened.
+        """
+        # Find the first occurence of the dongle serial port
+        try:
+            dongle_com_port = next(
+                port.device for port in serial.tools.list_ports.comports() if cls.DONGLE_DEVICE_NAME in port.description
+            )
+        except StopIteration:
+            raise serial.SerialException("Could not find dongle serial COM port")
+
+        # Open the serial connection
+        return serial.Serial(port=dongle_com_port, baudrate=baudrate)
 
     def timer_callback(self) -> None:
-        msg = String()
-        msg.data = "Hello World: %d" % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        # Trigger the reader to read all lines currently in the serial socket buffer
+        self.serial_line_reader.update()
+
+        while self.serial_line_reader.has_next_line():
+            btn_msg_raw = self.serial_line_reader.read_next_line().decode().strip()
+            self.get_logger().info("Received message: %s", btn_msg_raw)
+
+            ros_msg = std_msgs.msg.String(btn_msg_raw)
+            self.publisher.publish(ros_msg)
+
+    def command_specific_button(self, node_id: str, command: str) -> None:
+        command = f"{node_id}{self.COMMAND_SEPARATOR}{command}{self.COMMAND_END}"
+        self.serial_socket.write(command.encode())
+
+    def command_broadcast(self, command: str) -> None:
+        command = f"{self.BROADCAST_COMMAND}{self.COMMAND_SEPARATOR}{command}{self.COMMAND_END}"
+        self.serial_socket.write(command.encode())
 
 
 def main(args=None):
---
+++ ROS/bee_buttons/bee_buttons/periodic_serial_line_reader.py
@@ -0,0 +1,63 @@
+import serial
+
+
+class PeriodicSerialLineReader:
+    """
+    Serial line reader which can periodically read new lines.
+
+    This class is meant to be used in a loop where you want to periodically poll the serial socket for all
+    available data up to now and consume the read data later.
+    """
+
+    def __init__(self, serial_socket: serial.Serial):
+        """
+        Instantiate the PeriodicSerialLineReader.
+
+        :param serial_socket: Serial socket to read. The socket should be configured and opened.
+        :type serial_socket: PySerial `Serial` object
+        """
+        self.serial_socket = serial_socket
+        self.lines_buffer: list[bytearray] = []
+        self.partial_line_buffer = bytearray()
+
+    def update(self) -> None:
+        """
+        Read all contents from the serial socket's internal buffer and store read lines internally.
+
+        This function is meant to be called periodically.
+        """
+        # Read all available data on the socket
+        read_data = self.serial_socket.read(self.serial_socket.in_waiting)
+
+        # No data read means that a timeout has occurred
+        if not read_data:
+            raise serial.SerialException("Timeout on reading data")
+
+        # We might have a partial line left from the last iteration, so we need to prepend the read data with that
+        partial_line_buffer_extended_with_read_data = self.partial_line_buffer + read_data
+
+        # Split all data on the newline byte
+        read_lines = partial_line_buffer_extended_with_read_data.split(b"\n")
+
+        # If the last element in the read data was a newline, the last element of the returned read_lines list is empty.
+        # However, if the last element of the returned read_lines list is not empty, it contains a partial message.
+        if read_lines[-1]:
+            # Remove the partial message from the read_lines list and store in the partial line buffer
+            self.partial_line_buffer = read_lines.pop(-1)
+
+        # Add all full lines to the lines buffer
+        self.lines_buffer += read_lines
+
+    def has_next_line(self) -> bool:
+        """
+        Return whether there are full lines in the buffer.
+        """
+        return bool(self.lines_buffer)
+
+    def read_next_line(self) -> bytearray:
+        """
+        Returns the next line from the buffer. The line is also removed from the buffer.
+
+        You should first check with `has_next_line()` if there is a next line.
+        """
+        return self.lines_buffer.pop(1)
